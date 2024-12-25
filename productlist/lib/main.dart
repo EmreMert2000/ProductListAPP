0b82804fa52f0279085ff2b075a5c130132c8264
Emre Mert <61905534+EmreMert2000@users.noreply.github.com>
2024-12-25 19:20:14 +0300

    Update main.dart

--- productlist/lib/main.dart
+++ productlist/lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
-        title: 'Ürün Yönetimi',
+        title: 'Ürünler',
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
